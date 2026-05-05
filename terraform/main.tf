cd34fd59b26ca531f38bd6a7d09f0e96c51670ff
musaxasmammedov7 <musaxasmammedov7@icloud.com>
2026-05-05 22:03:59 +0400

    3

--- terraform/main.tf
+++ terraform/main.tf
@@ -93,7 +93,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = var.ssh_public_key
+    public_key = trimspace(var.ssh_public_key)
   }
 
   os_disk {
